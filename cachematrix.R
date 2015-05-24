fc3b787be66a4b03b1d49fd8b4062dc2553e2d9b
Naya Rhodes <irhodes@kent.edu>
2015-05-24 13:56:00 -0400

    Added comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,14 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## This project caches matrix inverses and returns them.
+
 ## Write a short comment describing this function
 
+## This function takes a matrix as an argument and stores it in mat. 
+## This function assigns NULL to the matrix inverse and initializes
+## the functions set, get, setsolve, and getsolve. These functions 
+## are then stored in a list.
 makeCacheMatrix <- function(x = matrix()) {
   matin <- NULL
   set <- function(mat) {
@@ -20,6 +26,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
+## This function takes x as an argument to access the cached data.
+## This function assigns the matrix inverse to the getsolve function
+## in the previous function. Then the matrix inverse is tested to
+## determine whether or not the inverse is NULL. If the cached inverse
+## exists then it is retrieved. If not then this function solves, sets, 
+## and outputs the inverse.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   matin <- x$getsolve()
